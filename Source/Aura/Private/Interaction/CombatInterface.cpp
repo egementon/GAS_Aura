1d4cf94f969aed9193addad9f3d54dd16c34c45c
Egemen Ton <egementon98@gmail.com>
2023-10-13 13:02:40 +0300

    Lecture 74

---
+++ Source/Aura/Private/Interaction/CombatInterface.cpp
@@ -0,0 +1,10 @@
+// Egemen Ton
+
+
+#include "Interaction/CombatInterface.h"
+
+// Add default functionality here for any ICombatInterface functions that are not pure virtual.
+int32 ICombatInterface::GetPlayerLevel()
+{
+	return 0;
+}
--- Source/Aura/Public/Character/AuraEnemy.h
+++ Source/Aura/Public/Character/AuraEnemy.h
@@ -23,8 +23,14 @@ public:
 	virtual void UnHighlightActor() override;
 	/* Enemy Interface*/
 
+	/* Combat Interface*/
+	virtual int32 GetPlayerLevel() override;
+	/* Combat Interface*/
+
 protected:
 	virtual void BeginPlay() override;
 	virtual void InitAbilityActorInfo() override;
 	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Character Class Defaults")
+	int32 Level = 1;
 };
