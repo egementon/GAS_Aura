a5f2f933fd0ccfdac0741258143dd43186df5729
Egemen Ton <113862655+egementon@users.noreply.github.com>
2024-01-15 15:23:45 +0300

    Lecture 155

---
+++ Source/Aura/Private/AbilitySystem/AuraAbilitySystemGlobals.cpp
@@ -0,0 +1,11 @@
+// Egemen Ton
+
+
+#include "AbilitySystem/AuraAbilitySystemGlobals.h"
+
+#include "AuraAbilityTypes.h"
+
+FGameplayEffectContext* UAuraAbilitySystemGlobals::AllocGameplayEffectContext() const
+{
+	return new FAuraGameplayEffectContext();
+}
